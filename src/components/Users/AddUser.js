f4a605af692421b772a64f7d6bfba7722cc26289
DarrelA <darrelaiscoding@gmail.com>
2022-04-21 18:03:09 +0800

    inputHandler() & addUserHandler()

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import { Button, Card } from '../UI';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [formData, setFormData] = useState({ name: '', age: '' });
+  const { name, age } = formData;
+
+  const inputHandler = (e) =>
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (name.trim().length === 0 || age.trim().length === 0) return;
+    if (+age < 1) return;
+
+    console.log(formData);
+    setFormData({ name: '', age: '' });
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" value={name} onChange={inputHandler} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" value={age} onChange={inputHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
